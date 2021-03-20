0f378dab5198d1ae70beba6cd509f21e7f9ed7fe
Tuuli TG <tuuli.k.toivanen@gmail.com>
2021-03-20 15:20:21 +0200

    tehtävä 10 tehty

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -2,19 +2,11 @@ package ohtu.verkkokauppa;
 
 public class Pankki implements PankkiInterface {
 
-    private static Pankki instanssi;
-
-    public static Pankki getInstance() {
-        if (instanssi == null) {
-            instanssi = new Pankki();
-        }
-
-        return instanssi;
-    }
+  
     private Kirjanpito kirjanpito;
 
-    public Pankki() {
-        kirjanpito = Kirjanpito.getInstance();
+    public Pankki(Kirjanpito k) {
+        kirjanpito = k;
     }
 
     @Override
