450001d3753b20c05a29cad05e702e3dcf376cd2
Tuuli TG <tuuli.k.toivanen@gmail.com>
2021-04-25 13:26:18 +0300

    lisäys

--- viikko7/t3-t4/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KPSPelaajaVsPelaaja.java
+++ viikko7/t3-t4/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KPSPelaajaVsPelaaja.java
@@ -2,7 +2,8 @@ package ohtu.kivipaperisakset;
 
 
 public class KPSPelaajaVsPelaaja extends KiviPaperiSakset {
-
+    
+    
     @Override
     protected String toisenSiirto() {
         System.out.println("Toisen pelaajan siirto: ");
